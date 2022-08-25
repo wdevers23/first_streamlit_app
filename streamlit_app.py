f4b08b5a8324b03457b20b113251bcb360698783
wdevers23 <80409986+wdevers23@users.noreply.github.com>
2022-08-25 14:11:18 -0600

    Update streamlit_app.py
    Added pre-picked fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #streamlit.dataframe(my_fruit_list)
 #Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index),['Avocado','Strawberries'])
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
