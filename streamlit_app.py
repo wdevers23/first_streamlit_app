9b8af3604fb8024e87bb0005007476d8d72e33e1
wdevers23 <80409986+wdevers23@users.noreply.github.com>
2022-08-25 14:07:32 -0600

    Update streamlit_app.py
    Added multiselect to page

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,4 +13,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#streamlit.dataframe(my_fruit_list)
+#Let's put a pick list here so they can pick the fruit they want to include
+streamlit.multiselect("Pick some fruits: ", list(my_fruit_list.index))
+#display the table on the page
 streamlit.dataframe(my_fruit_list)
