1e355c9a96f00d889778f4f62676c23a0a2d7ec7
wdevers23 <80409986+wdevers23@users.noreply.github.com>
2022-08-26 13:54:46 -0600

    Update streamlit_app.py
    Add fruit choice text input and display the choices

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,8 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # Normalize Json data into readable text 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
