84b36bf5d680e512148c0112d79a409edb30320c
wdevers23 <80409986+wdevers23@users.noreply.github.com>
2022-08-29 09:15:51 -0600

    Update streamlit_app.py
    Remove extra indent

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 def get_fruityvice_data(this_fruit_choice):
    fruitvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
   
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
